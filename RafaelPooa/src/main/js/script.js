6d96ecacebc9d09e4a94f6109ff9f708203c37d8
Rafael Caçador Dioscânio <141187927+rafaelCacador@users.noreply.github.com>
2024-02-19 20:18:19 -0300

    Add files via upload

---
+++ RafaelPooa/src/main/js/script.js
@@ -0,0 +1,20 @@
+
+    //var btn = document.querySelectorAll
+    var btn = document
+        .querySelector(".row label:nth-child(5) button");
+
+    var campos = document
+        .querySelectorAll(".row input");
+
+       const fnt = () ==>{
+       if(campos[0].value != " "
+       && campos[1].value != " "){
+          btn.style.visibility = "show"
+       }else{
+          btn.style.visibility = "hidden"}
+         }
+        campos.forEach(i -> i.charge = fnt)
+console.log(btn);
+//btn.style.display = "none";
+btn.style.visibility = "hidden";
+console.log(campos);
